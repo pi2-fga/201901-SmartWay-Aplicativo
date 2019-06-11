d361d98c8c0a2e7d286fdc1c02ee563e6185249b
Renatafsouza <renata.rfsouza@gmail.com>
2019-06-10 22:48:57 -0300

    Generating directions map google

--- smartway/android/app/src/main/java/com/smartway/MainApplication.java
+++ smartway/android/app/src/main/java/com/smartway/MainApplication.java
@@ -3,13 +3,13 @@ package com.smartway;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.mapbox.rctmgl.RCTMGLPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
           new RNCameraPackage(),
-          new RCTMGLPackage(),
           new RNSoundPackage()
       );
     }
